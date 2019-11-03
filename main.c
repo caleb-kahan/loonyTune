314a53e785d699b35e8b1050f083bfad809dab3c
Caleb Kahan <ckahan00@stuy.edu>
2019-11-03 10:53:17 -0500

    cretaing song_cmp

--- main.c
+++ main.c
@@ -11,18 +11,25 @@ struct Song_node * insert_front(struct Song_node *nody, char *name, char *artist
     newSong->next = nody;
     return newSong;
 }
-struct Song_node * insert_ordered(struct Song_node *nody, int data){
+int song_cmp(struct Song_node *one, struct Song_node *two){
+  if(str_cmp(one->name,two->name)){
+    return str_cmp(one->artist,two->artist)){
+  }
+  return str_cmp()
+}
+struct Song_node * insert_ordered(struct Song_node *front, struct Song_node *newNode){
     struct Song_node *newSong = malloc(sizeof(struct Song_node));
+    *newSong = *newNode;
+    //newSong->name = newNode->name;
+    //newSong->artist = newNode->artist;
+    //newSong->next = newNode->next;
+    struct node *currentNode = front;
     while(currentSong_node->next){
-      if(currentSong_node->next->i == data){
+      if(strcmp(currentSong_node->name, newSong ->name)){
           struct Song_node *deadSong_node = currentSong_node->next;
-          if(currentSong_node->next->next)
-            currentSong_node->next = currentSong_node->next->next;
-          else
-            currentSong_node->next = NULL;
-          free(deadSong_node);
           return front;
       }
+      if(strcmp())
       currentSong_node = currentSong_node->next;
     }
     newSong->i = data;
