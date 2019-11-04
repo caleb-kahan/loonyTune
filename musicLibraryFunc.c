89e56007c4064e66d298d40bffc8743c3a3a4a6a
Caleb Kahan <ckahan00@stuy.edu>
2019-11-03 23:19:06 -0500

    coming bakc soon

--- musicLibraryFunc.c
+++ musicLibraryFunc.c
@@ -16,14 +16,44 @@ struct Song_node * [] add_song(Song_node *[] library, Song_node *songy){
   return library;
 }
 struct Song_node * song_search(Song_node *[] library, char *artist, char *name){
-  int index = findIndex(*songy);
+  int index = findIndex(artist[0]);
   return find_unique(library[unique], artist, name);
 }
+struct song_node * artist_search(Song_node *[] library, char *artist){
+  int index = findIndex(artist[0]);
+  struct song_node *front = library[index];
+  return find_artist(front,artist);
+}
+void print_letter(Song_node *[] library, char c){
+  int index = findIndex(c);
+  print_list(library[index]);
+}
+void print_artist(Song_node *[] library, char *artist){
+  struct song_node *start = artist_search(library,artist);
 
-int findIndex(struct_node *songy){
-  int index = songy[0] - 'a';
+  while(start){
+    print_node(start);
+    printf("\n");
+    start = start->next;
+    start = find_artist(start,artist);
+  }
+}
+void print_library(Song_node *[] library){
+  char a;
+  for(a='a';a<='z'+1;a++){
+    print_letter(library,a);
+  }
+}
+int findIndex(struct song_node *nody){
+  return findIndex((nody->artist)[0]);
+}
+int findIndex(char c){
+  int index = c - 'a';
   if(! (index >= 0 && index <26)){
     index = 26;
   }
   return index;
+}
+void shuffle(){
+  
 }
